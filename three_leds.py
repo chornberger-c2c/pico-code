2b139549e1ec00b8077134f1e19d06447edce0db
horni <christopher.hornberger@gmail.com>
2021-10-21 19:32:24 +0200

    linting

--- three_leds.py
+++ three_leds.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python
 
+"""
+control a sequence of led lights on the Rasperry Pi Pico (turned on or off)
+"""
+
 import machine
 import utime
 
+# assign each color of led to its pin number
 pins = {
-  "red": 15,
-  "yellow": 14,
-  "green": 13,
+    "red": 15,
+    "yellow": 14,
+    "green": 13,
 }
 
 def switch(color, mode):
+    """
+    switch a led with a certain color on or off
+    """
     turn = 1 if mode == "on" else 0
     machine.Pin(pins[color], machine.Pin.OUT).value(turn)
     utime.sleep(2)
 
+
+# do the loop sequence
 while True:
     switch("red", "on")
     switch("red", "off")
